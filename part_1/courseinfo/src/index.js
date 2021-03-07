a11d9c0bab5d84441c550a6f3fbe7567a766d58d
luciaoSainz <danielsainz28@gmail.com>
2021-03-07 17:18:10 +0100

    added total exercises in index.js

--- part_1/courseinfo/src/index.js
+++ part_1/courseinfo/src/index.js
@@ -27,6 +27,14 @@ const Contet = (props) => {
   )
 }
 
+const Total = (props) => {
+  return(
+    <>
+    <p>Number of exercises {props.total[0].exercises + props.total[1].exercises + props.total[2].exercises}</p>
+    </>
+  )
+}
+
 const App = () => {
   const course = {
     name: 'Half Stack application development',
