5f7995744903adc9d11013239fcc295601725641
luciaoSainz <danielsainz28@gmail.com>
2021-03-21 15:15:58 +0100

    added buttons with value good,neutral and bad

--- part_1/unicafe/src/index.js
+++ part_1/unicafe/src/index.js
@@ -2,13 +2,33 @@ import React, {useState} from 'react';
 import ReactDOM from 'react-dom';
 
 
-
+const Statistics = (props) => {
+  if(props.suma === 0 && props.title === true) {
+    return(
+       <tr>
+         <td>No comments provided</td>
+       </tr>
+    )
+  }
+  if(props.suma === 0){
+    return(
+      <tr>
+        <td></td>
+      </tr>
+    )
+  }
+  return(
+    <tr>
+      <td>{props.name} {props.val}
+      </td>
+    </tr>
+  )
+}
 
 
 const Button = ({handleClick, text}) => (
   <button onClick={handleClick}>{text}</button>
 )
-
 
 const App = () => {
   // save clicks of each button to its own state
@@ -30,10 +50,27 @@ const App = () => {
 
   return (
     <div>
+      <div>
+      <h2>give feedback</h2>
       <Button handleClick={handleGoodClick} text='Good' />
       <Button handleClick={handleNeutralClick} text='Neutral' />
       <Button handleClick={handleBadClick} text='Bad' />
+  
+
+    <h2>Statistics</h2>
+    <table>
+      <tbody>
+    <Statistics text={true} suma={good + bad + neutral} />
+    <Statistics text="good" value ={good + bad + neutral} val={good} />
+    <Statistics text="neutral" value ={good + bad + neutral} val={neutral} />
+    <Statistics text="bad" value ={good + bad + neutral} val={bad} />
+
+   
+      </tbody>
+    </table>  
+      </div>
     </div>
+
   )
 }
 
