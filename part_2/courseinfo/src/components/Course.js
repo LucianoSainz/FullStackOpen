9cfb919dafd5375c7be024186f586c14f1dc86fa
LucianoSainz <danielsainz28@gmail.com>
2021-04-27 18:09:39 +0200

    added in Course header, content and total

--- part_2/courseinfo/src/components/Course.js
+++ part_2/courseinfo/src/components/Course.js
@@ -2,31 +2,41 @@ import Reat from 'react';
 
 
 const Header = props => {
-      <h2>{props.course}</h2>
-     
+    return(
+      <h1>{props.course.name}</h1>
+    )
 }
 
 const Total = (props) => {
     const parts = props.course.parts.map(course => course.exercises)
     return(
-        <p>there are in total {parts.reducer((s, p) => s + p)}</p>
+        <p>there are in total {parts.reduce((s, p) => s + p)}</p>
     )
 }
 
-const Content = () => {
-
-}
-
 const Part = props => {
-    <p>{props.name} {props.exercises}</p>
+    return(
+         <p>{props.name} {props.exercises}</p>
+    )
+    
+ }
 
+const Content = (props) => {
+  return(
+      <div>
+          {props.course.parts.map(part => <Part key={part.id} name={part.name} exercises={part.exercises} />)}
+      </div>
+   )
 }
 
 
-const Course = () =>{
+
+const Course = (props) =>{
     return(
         <div>
             <Header course={props.course} />
+            <Content course={props.course} />
+            <Total course={props.course} />
         </div>
     )
 
