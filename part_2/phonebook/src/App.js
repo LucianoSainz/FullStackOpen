b51da36ac8dcbdadd1a71b89c40c5ae66ffe9321
LucianoSainz <danielsainz28@gmail.com>
2021-05-24 16:00:18 +0200

    added handleClick and onChange

--- part_2/phonebook/src/App.js
+++ part_2/phonebook/src/App.js
@@ -1,26 +1,38 @@
+import React, { useState } from 'react';
 
 
-import React, { useState } from 'react'
-
-const App = () => {
-  const [ persons, setPersons ] = useState([
-    { name: 'Arto Hellas' }
-  ]) 
+const App = (props) => {
+  const [ persons, setPersons ] = useState(props.persons) 
   const [ newName, setNewName ] = useState('')
+
+  const handleChange = (event) => {
+    setNewName(event.target.value)
+  }
+
+  const handleClick = (event) => {
+    console.log('crear nota');
+    const addNewName = {
+      name: newName,
+      id:Math.floor(Math.random() * 101)
+    }
+    console.log(addNewName);
+
+    setNewName()
+
+ }
 
   return (
     <div>
       <h2>Phonebook</h2>
       <form>
         <div>
-          name: <input />
+          name: <input  type='text' onChange={handleChange} />
         </div>
         <div>
-          <button type="submit">add</button>
+          <button type="submit" onClick={handleClick}>add</button>
         </div>
       </form>
       <h2>Numbers</h2>
-      ...
     </div>
   )
 }
