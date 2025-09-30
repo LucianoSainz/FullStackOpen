0bf147dd98a337b987509b81e3f700dacb589728
LucianoSainz <danielsainz28@gmail.com>
2025-09-30 23:36:25 +0200

    modifying files and checking functionality

---
+++ part1/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+const App = () => {
+
+  return (
+    
+      <div>
+       <p>Probando el funcionamiento de vite con React</p>
+    </div>
+  )
+}
+
+export default App
---
+++ part1/src/main.jsx
@@ -0,0 +1,8 @@
+
+import ReactDOM from 'react-dom/client'
+
+import App from './App.jsx'
+
+createRoot(document.getElementById('root')).render(
+    <App />
+)
